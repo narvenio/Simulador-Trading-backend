720555b997bff65e4d092a6e6f55272da86088f0
Jesus Daniel Gonzalez Bautista <121988244+narvenio@users.noreply.github.com>
2026-06-04 18:00:59 -0400

    Add files via upload

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "sqlite:///./trading.db"
+import os
+#DATABASE_URL = "sqlite:///./trading.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
